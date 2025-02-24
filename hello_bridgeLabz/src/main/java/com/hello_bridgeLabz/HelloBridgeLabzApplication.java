7b5c05db0dff1c3252cc73ef4f27df45aea1206f
Sahiljain24 <24sahilj@gmail.com>
2025-02-24 13:09:14 +0530

    [Sahil] Add . Added all the files for UC_4

--- hello_bridgeLabz/src/main/java/com/hello_bridgeLabz/HelloBridgeLabzApplication.java
+++ hello_bridgeLabz/src/main/java/com/hello_bridgeLabz/HelloBridgeLabzApplication.java
@@ -1,13 +1,17 @@
 package com.hello_bridgeLabz;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class HelloBridgeLabzApplication {
 
-	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 		SpringApplication.run(HelloBridgeLabzApplication.class, args);
+
 	}
 
 }
